da0056ec6b5ff673c89025143b850425809b4e21
shun-p0430 <chocosensei@gmail.com>
2022-03-16 17:20:53 +0900

    ログイン機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :nickname, :birthday, presence: true
+  
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX
 
-  validates :nickname, :first_name, :last_name, :first_name_ruby, :last_name_ruby, :birthday, presence: true
+  with_options presence: true, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください'} do
+    validates :first_name
+    validates :last_name
+  end
+
+  with_options presence: true, format: {with: /\A[ァ-ヶー]+\z/, message: '全角カナを使用してください'} do
+    validates :first_name_ruby
+    validates :last_name_ruby
+  end
+
 end
