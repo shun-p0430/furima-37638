19ab7baf17430d98f9e87e6b131b395e16a9f080
shun-p0430 <chocosensei@gmail.com>
2022-03-17 17:58:38 +0900

    エラーハンドリングの表示2

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,8 +11,7 @@ class User < ApplicationRecord
     validates :nickname
     validates :first_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters'}
     validates :last_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters'}
-    validates :first_name_ruby, format: {with: /\A[ァ-ヶー]+\z/, message: 'is invalid. Input full-width katakana characters
-      '}
+    validates :first_name_ruby, format: {with: /\A[ァ-ヶー]+\z/, message: 'is invalid. Input full-width katakana characters'}
     validates :last_name_ruby, format: {with: /\A[ァ-ヶー]+\z/, message: 'is invalid. Input full-width katakana characters'}
     validates :birthday
   end
