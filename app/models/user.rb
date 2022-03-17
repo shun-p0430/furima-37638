b609425fb36665d03f0c8c9dfdc4c1943eb563c0
shun-p0430 <chocosensei@gmail.com>
2022-03-17 17:21:11 +0900

    エラーハンドリングの表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,20 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
-  validates :nickname, :birthday, presence: true
-  
+    
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'is invalid. Include both letters and numbers'
 
-  with_options presence: true, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください'} do
-    validates :first_name
-    validates :last_name
-  end
-
-  with_options presence: true, format: {with: /\A[ァ-ヶー]+\z/, message: '全角カナを使用してください'} do
-    validates :first_name_ruby
-    validates :last_name_ruby
+  with_options presence: true do
+    validates :nickname
+    validates :first_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters'}
+    validates :last_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters'}
+    validates :first_name_ruby, format: {with: /\A[ァ-ヶー]+\z/, message: 'is invalid. Input full-width katakana characters
+      '}
+    validates :last_name_ruby, format: {with: /\A[ァ-ヶー]+\z/, message: 'is invalid. Input full-width katakana characters'}
+    validates :birthday
   end
 
 end
