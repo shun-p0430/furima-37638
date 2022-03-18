e043021da4057b752b5309bc7981443ff0ec33db
shun-p0430 <chocosensei@gmail.com>
2022-03-18 17:06:29 +0900

    アクティブハッシュ、バリデーションの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,14 @@
 class ItemsController < ApplicationController
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new
+    if @item.save
+      redirect_to :index
+    else
+      render :new
+    end
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,13 +2,23 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :name
     validates :text
+    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: 'is out of setting range' }
+  end
+
+  with_options numericality: { other_than: 0, message: "can't be blank" } do
     validates :category_id
     validates :status_id
     validates :postage_id
     validates :prefecture_id
     validates :shipping_date_id
-    validates :price
   end
 
   belongs_to :user
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :postage
+  belongs_to :prefecture
+  belongs_to :shipping_date
 end
