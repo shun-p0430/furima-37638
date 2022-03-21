404f68d724bfc6220e441902f70ad3e6e1893d99
shun-p0430 <chocosensei@gmail.com>
2022-03-21 16:55:32 +0900

    Itemモデルテスト修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -114,6 +114,12 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Image can't be blank")
       end
+
+      it 'user_idが空では出品できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include('User must exist')
+      end
     end
   end
 end
