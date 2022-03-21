dba64685e986c57eeac65763e20944860d9c0efb
shun-p0430 <chocosensei@gmail.com>
2022-03-21 12:03:15 +0900

    出品ページの作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,11 +4,18 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item = Item.new
+    @item = Item.new(item_params)
     if @item.save
-      redirect_to :index
+      redirect_to root_path
     else
       render :new
     end
   end
+
+  private
+  
+  def item_params
+    params.require(:item).permit(:name, :text, :category_id, :status_id, :postage_id, :prefecture_id, :shipping_date_id, :price, :image).merge(user_id: current_user.id)
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,9 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :name
     validates :text
-    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: 'is out of setting range' }
+    validates :image
+    validates :price, numericality: { only_integer: true, message: 'invalid. Input half-width characters' }
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: 'is out of setting range' }
   end
 
   with_options numericality: { other_than: 0, message: "can't be blank" } do
@@ -14,6 +16,7 @@ class Item < ApplicationRecord
   end
 
   belongs_to :user
+  has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
