b5f1048c7d54675a514f704fc70966df259cee9a
shun-p0430 <chocosensei@gmail.com>
2022-03-22 16:06:59 +0900

    商品情報編集ページの作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: :new
+  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :move_to_index, only: :edit
 
   def index
     @items = Item.order('created_at DESC')
@@ -22,10 +23,29 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(params[:id])
+    else
+      render :edit
+    end
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:name, :text, :category_id, :status_id, :postage_id, :prefecture_id, :shipping_date_id, :price,
                                  :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless params[:user_id] == current_user.id
+      redirect_to root_path
+    end
   end
 end
