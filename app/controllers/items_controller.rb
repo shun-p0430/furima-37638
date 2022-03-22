5e8452fa489015879db7393bbb101f5b419e5142
shun-p0430 <chocosensei@gmail.com>
2022-03-22 18:31:30 +0900

    コメントアウト削除、destroyにbefore_action追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :authenticate_user!, only: [:new, :edit, :destroy]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :move_to_index, only: [:edit, :destroy]
 
@@ -24,7 +24,6 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    # redirect_to root_path unless @item.user_id == current_user.id
   end
 
   def update
