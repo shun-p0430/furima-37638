971a57bc3430b0e18acba7f8c448a26de30924c9
shun-p0430 <chocosensei@gmail.com>
2022-03-22 16:07:49 +0900

    Rubocopの実行

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -44,8 +44,6 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    unless params[:user_id] == current_user.id
-      redirect_to root_path
-    end
+    redirect_to root_path unless params[:user_id] == current_user.id
   end
 end
